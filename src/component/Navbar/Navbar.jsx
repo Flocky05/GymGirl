94070168fd71592ede84af21164eb78a1bd6ac9e
Flocky05 <md.rakibul.islam.0501@gmail.com>
2023-10-31 23:59:48 +0600

    added navbar section

--- src/component/Navbar/Navbar.jsx
+++ src/component/Navbar/Navbar.jsx
@@ -1,9 +1,21 @@
-
+import { Link } from "react-router-dom";
+import img from "../../assets/Logo.png"
 
 const Navbar = () => {
     return (
         <div>
-            <h2>This is navbar section</h2>
+            <div className="flex justify-between">
+                <img src={img} alt="" />
+                <div>
+                    <Link to="/">Home</Link>
+                    <Link to="/">Benefits</Link>
+                    <Link to="/">Our Class</Link>
+                    <Link to="/">Contact us</Link>
+                </div>
+            </div>
+            <div>
+                
+            </div>
         </div>
     );
 };
