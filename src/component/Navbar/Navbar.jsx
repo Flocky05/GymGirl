6a50b58c471b99f3aa66dee955e592f569d863f3
Flocky05 <md.rakibul.islam.0501@gmail.com>
2023-11-01 00:12:23 +0600

    update navbar section

--- src/component/Navbar/Navbar.jsx
+++ src/component/Navbar/Navbar.jsx
@@ -3,18 +3,19 @@ import img from "../../assets/Logo.png"
 
 const Navbar = () => {
     return (
-        <div>
-            <div className="flex justify-between">
+        <div className="flex justify-between items-center px-8 p-4 hover:bg-red-200">
+            <div className="grid grid-cols-2 gap-20">
                 <img src={img} alt="" />
-                <div>
+                <div className="grid grid-cols-4">
                     <Link to="/">Home</Link>
-                    <Link to="/">Benefits</Link>
-                    <Link to="/">Our Class</Link>
-                    <Link to="/">Contact us</Link>
+                    <Link to="/benefits">Benefits</Link>
+                    <Link to="/our_class">Our Class</Link>
+                    <Link to="/contact_us">Contact us</Link>
                 </div>
             </div>
-            <div>
-                
+            <div className="flex gap-4 items-center justify-center">
+                <Link to="/sign_in">Sign in</Link>
+                <Link className="bg-yellow-400 px-7 py-2 rounded-md mt-3 inline-block" to="/new_member">Become a Mamber</Link>
             </div>
         </div>
     );
