a31d4ffc139c15eb56bf58f9466722430c4c6604
Flocky05 <md.rakibul.islam.0501@gmail.com>
2023-11-01 14:48:41 +0600

    update contact section

--- src/component/Contact/Contact.jsx
+++ src/component/Contact/Contact.jsx
@@ -6,7 +6,7 @@ const Contact = () => {
         <div className="p-20">
             <h2 className="text-3xl font-bold uppercase py-4"> <span className="text-pink-500">Join now </span> to get in shape</h2>
             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Omnis doloremque modi, voluptate aut quis quisquam tempora incidunt laudantium obcaecati repellendus voluptatibus tempore esse quas eveniet unde recusandae. Consequatur, inventore laudantium?</p>
-            <div className="grid grid-cols-1 lg:grid-cols-2 justify-items-center mt-20">
+            <div className="lg:flex justify-center  mt-20">
                 <form action="">
                     <input type="text" placeholder="Name" className="bg-red-200 py-2 rounded-md w-3/5 p-2 mt-4"/>
                     <input type="text" placeholder="Email" className="bg-red-200 py-2 rounded-md w-3/5 my-3 p-2"/>
@@ -14,7 +14,7 @@ const Contact = () => {
                     <br />
                     <button className="bg-yellow-400 px-6 py-2 rounded-md">Submit</button>
                 </form>
-                <div className="relative">
+                <div className="relative mt-20 lg:mt-0">
                     <img src={img} alt="" />
                     <img  className="absolute -mt-8 -ml-8 -z-10 w-max" src={img2} alt="" />
                 </div>
